12ea9f384ee9daead6523935409a3ad28b80d87a
Julius Ramos <juliuscervantesramos@gmail.com>
2023-12-04 10:38:30 +0800

    Start Application

---
+++ app/src/main/java/com/ramos/juliusadrian/block6/p1/quiz/SignUpPage.kt
@@ -0,0 +1,11 @@
+package com.ramos.juliusadrian.block6.p1.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_page)
+    }
+}
